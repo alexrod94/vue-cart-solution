10ab51096b71972c698d6d1c2fa4ba7dd5cdc6a6
alexrod94 <alexrod94@gmail.com>
2022-12-06 18:54:49 +0000

    First v-for

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,32 +1,14 @@
 <template>
   <div id="card" class="flex flex-col">
-    <img :src="item.img" class="w-full h-[250px] object-cover" />
-    <h3 class="text-2xl mt-5">{{ item.name }}</h3>
-    <p class="mt-2">{{ item.price }}€</p>
+    <img src="/" class="w-full h-[250px] object-cover" />
+    <h3 class="text-2xl mt-5">Cerveza</h3>
+    <p class="mt-2">20€</p>
     <button class="text-white py-1 px-6 mt-3 w-full bg-green-500 rounded">
       Add to cart
     </button>
   </div>
 </template>
 
-<script setup>
-import { ref } from "vue";
-const item = ref({
-  img: "",
-  name: "",
-  price: "",
-});
-
-async function getBeer() {
-  const res = await fetch("https://api.punkapi.com/v2/beers");
-  const finalRes = await res.json();
-
-  item.value.img = finalRes[0].image_url;
-  item.value.name = finalRes[0].name;
-  item.value.price = finalRes[0].ibu;
-}
-
-getBeer();
-</script>
+<script setup></script>
 
 <style></style>
